da1bfd43819293bb428738246e121a28c4fb91f3
Aslam youseph <aslamyouseph@gmail.com>
2025-02-17 11:33:21 +0530

    Admin Delete Update HowToDO page added

--- client/src/Admin-Components/PageAdmin/AdminDeleteUpdateHowToDo.js
+++ client/src/Admin-Components/PageAdmin/AdminDeleteUpdateHowToDo.js
@@ -7,7 +7,7 @@ function AdminDeleteUpdateHowToDo() {
       {/*This is used to import the admin Navigation bar  form Admin folder */}
       <AdminNavBar />
       <DeleteUpdateHowToDo />{" "}
-      {/*This is used to import the delete update how to do page*/}
+      {/*This is used to import the Delete update how to do page*/}
     </div>
   );
 }
