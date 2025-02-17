b7719e50ebaeb92dae42ce648ca8647487c83703
Aslam youseph <aslamyouseph@gmail.com>
2025-02-17 18:19:20 +0530

    Main Route is added

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App"; // This is used to import the app page
 import { BrowserRouter as Router } from "react-router-dom"; // This is used to enable routing in react example (http://localhost:3000/page1)
 import "bootstrap/dist/css/bootstrap.min.css"; // This code is used to import the bootstrap in our project
 const root = ReactDOM.createRoot(document.getElementById("root"));
