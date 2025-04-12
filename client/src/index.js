b9d7ade0ccb4df88c62e813e1970fc598c302719
Aslam youseph <aslamyouseph@gmail.com>
2025-04-12 14:49:44 +0530

    Contact API added for search operation

--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App"; // This is used to import the app page
 import { BrowserRouter as Router } from "react-router-dom"; // This is used to enable routing in react example (http://localhost:3000/page1)
 import "bootstrap/dist/css/bootstrap.min.css"; // This code is used to import the bootstrap in our project
+import { SearchProvider } from "./SearchContext.js"; // This is used to create a React Context API to share the search input value across multiple components in your app
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   // This code is used to give the router to the app page
   <React.StrictMode>
     <Router>
-      <App />
+      {/* When we can the App during that time itself the context will be created */}
+      <SearchProvider>
+        <App />
+      </SearchProvider>
     </Router>
   </React.StrictMode>
 );
