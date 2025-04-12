2fe2cab6359b80c11caf533f5a145d962cb4a310
Aslam youseph <aslamyouseph@gmail.com>
2025-04-12 14:52:31 +0530

    Search bar is created

--- client/src/Admin-Components/PageAdmin/AdminDeleteUpdateHowToDo.js
+++ client/src/Admin-Components/PageAdmin/AdminDeleteUpdateHowToDo.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AdminNavBar from "../AdminSideBar/AdminSideBar";
 import DeleteUpdateHowToDo from "../ManageHowToDo/AdminDeleteUpdateHowToDo";
+import SearchBar from "../../User-Components/Home/SearchBar/searchBar";
 function AdminDeleteUpdateHowToDo() {
   return (
     <div>
       {/*This is used to import the admin Navigation bar  form Admin folder */}
       <AdminNavBar />
+      <SearchBar /> {/*This is used to import the Search bar  */}
       <DeleteUpdateHowToDo />{" "}
       {/*This is used to import the Delete update how to do page*/}
     </div>
