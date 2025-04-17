8a17305c21ec66262fe71a43af6ef22207a033f3
Aslam youseph <aslamyouseph@gmail.com>
2025-04-17 19:15:40 +0530

    Data imported to the MongoDB Atlas

--- server/DB-Configaration/DataBaseConnection.js
+++ server/DB-Configaration/DataBaseConnection.js
@@ -1,16 +1,20 @@
 /*This is the connection code - to connect to the mongoDB  */
 
 const mongoose = require("mongoose");
-
+require("dotenv").config();
+// In here we connected to the mongoDB Atlas . Atlas which is mainly using for storing the data in cloud
+// mongoDB Compose is used to store the data locally on our devices
+// during hosting time always the data needed to store in mongoDB Atlas
 const connectDB = async () => {
+  // MONGODB_URI => value is available in the .env file
   try {
-    await mongoose.connect("mongodb://localhost:27017/MainProject", {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("Connected to MongoDB");
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("❌ MongoDB connection error:", error);
     process.exit(1);
   }
 };
